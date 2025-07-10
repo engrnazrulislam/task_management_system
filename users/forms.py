c86557d6c0a9adefd9f03aad864dce5a6dc1ecd9
Mr. Md. Nazrul Islam <57587377+engrnazrulislam@users.noreply.github.com>
2025-07-10 21:30:15 +0600

    implement registration form

--- users/forms.py
+++ users/forms.py
@@ -25,15 +25,66 @@ class CustomRegistrationForm(forms.ModelForm):
         model = User
         fields = ['username','first_name','last_name','password1','confirmed_password','email']
 
-        # Now we make some clean method for validation
+    # Now we make some clean method for validation
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        
+        errors = []
+        # Condition for error checking
+        if len(password1) < 8:
+            # raise forms.ValidationError('Password must be 8 character long')
+            errors.append('Password must be 8 character long')
+         # Home work implement regular expression
 
-        def clean_password1(self):
-            password1 = self.cleaned_data.get('password1')
+        if re.search(r'[A-Z]',password1):
+            raise forms.ValidationError('Password does not contain any capital letter')
 
-            if len(password1) < 8:
-                 raise forms.ValidationError('Password must be 8 character long')
-            
-            if re.fullmatch(r'A-Za-z0-9#$%@+='):
-                 raise forms.ValidationError('Password must contain uppercase, lowercase, min 8 character')
-            
+        if re.search(r'[a-z]',password1):
+            raise forms.ValidationError('Password does not contain small letter')
+
+        if re.search(r'[0-9]',password1):
+            raise forms.ValidationError('Password does not contain any number')
+        
+        if re.search(r'[@*#$+=]',password1):
+            raise forms.ValidationError('Password does not contain any special character')
+        
+
+
+
+        if "abc" not in password1:
+            errors.append('Password not contain abc')
+        
+        if errors:
+            raise forms.ValidationError(errors)
+        
+        return password1
+
+        # if re.fullmatch(r'[A-Za-z0-9#$%@^+=]',password1):
+        #         raise forms.ValidationError('Password must contain uppercase, lowercase, min 8 character')
+    # non-field validataion
+    def clean(self):
+        clean_data = super().clean()
+        password = clean_data.get('password1')
+        confirmed_password = clean_data.get('confirmed_password')
+
+        if password and confirmed_password and password != confirmed_password:
+            raise forms.ValidationError('Password does not match')
+
+        return clean_data
+
+    # Home work email validation
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_exists = User.objects.filter(email=email).exists()
+        if email_exists:
+            raise forms.ValidationError('Email already exists')
+        
+        return email
+    
+
+   
+
+
+    
+
 
